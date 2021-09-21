05344cdfcb05596a301c31f0710549ac12c7d43a
Pablo <pablo.jorda@allfunds.com>
2021-09-21 19:02:35 +0200

    boilerplate reduction

--- src/index.js
+++ src/index.js
@@ -54,48 +54,73 @@ const SignupForm = () => {
     },
   });
   return (
+    // <form onSubmit={formik.handleSubmit}>
+    //   <label htmlFor="firstName">First Name</label>
+    //   <input
+    //     id="firstName"
+    //     name="firstName"
+    //     type="text"
+    //     onChange={formik.handleChange}
+    //     onBlur={formik.handleBlur}
+    //     value={formik.values.firstName}
+    //   />
+    //   {formik.touched.firstName && formik.errors.firstName ? (
+    //     <div>{formik.errors.firstName}</div>
+    //   ) : null}
+
+    //   <label htmlFor="lastName">Last Name</label>
+    //   <input
+    //     id="lastName"
+    //     name="lastName"
+    //     type="text"
+    //     onChange={formik.handleChange}
+    //     onBlur={formik.handleBlur}
+    //     value={formik.values.lastName}
+    //   />
+    //   {formik.touched.lastName && formik.errors.lastName ? (
+    //     <div>{formik.errors.lastName}</div>
+    //   ) : null}
+
+    //   <label htmlFor="email">Email Address</label>
+    //   <input
+    //     id="email"
+    //     name="email"
+    //     type="email"
+    //     onChange={formik.handleChange}
+    //     onBlur={formik.handleBlur}
+    //     value={formik.values.email}
+    //   />
+    //   {formik.touched.email && formik.errors.email ? (
+    //     <div>{formik.errors.email}</div>
+    //   ) : null}
+
+    //   <button type="submit">Submit</button>
+    // </form>
     <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="firstName">First Name</label>
-      <input
-        id="firstName"
-        name="firstName"
-        type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.firstName}
-      />
-      {formik.touched.firstName && formik.errors.firstName ? (
-        <div>{formik.errors.firstName}</div>
-      ) : null}
-
-      <label htmlFor="lastName">Last Name</label>
-      <input
-        id="lastName"
-        name="lastName"
-        type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.lastName}
-      />
-      {formik.touched.lastName && formik.errors.lastName ? (
-        <div>{formik.errors.lastName}</div>
-      ) : null}
-
-      <label htmlFor="email">Email Address</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.email}
-      />
-      {formik.touched.email && formik.errors.email ? (
-        <div>{formik.errors.email}</div>
-      ) : null}
-
-      <button type="submit">Submit</button>
-    </form>
+       <label htmlFor="firstName">First Name</label>
+       <input
+         id="firstName"
+         type="text"
+         {...formik.getFieldProps('firstName')}
+       />
+       {formik.touched.firstName && formik.errors.firstName ? (
+         <div>{formik.errors.firstName}</div>
+       ) : null}
+ 
+       <label htmlFor="lastName">Last Name</label>
+       <input id="lastName" type="text" {...formik.getFieldProps('lastName')} />
+       {formik.touched.lastName && formik.errors.lastName ? (
+         <div>{formik.errors.lastName}</div>
+       ) : null}
+ 
+       <label htmlFor="email">Email Address</label>
+       <input id="email" type="email" {...formik.getFieldProps('email')} />
+       {formik.touched.email && formik.errors.email ? (
+         <div>{formik.errors.email}</div>
+       ) : null}
+ 
+       <button type="submit">Submit</button>
+     </form>
   );
 };
 
